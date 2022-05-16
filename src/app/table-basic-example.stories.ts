b3075071e4adbc72a2fede947f055a04f2ff40b7
Trevor Dearham <trevor.dearham@gmail.com>
2022-05-16 16:51:50 +0100

    Found a complete workaround. Expored other options

--- src/app/table-basic-example.stories.ts
+++ src/app/table-basic-example.stories.ts
@@ -20,12 +20,67 @@ const template: Story<TableBasicExample> = (args: TableBasicExample) => ({
   },
 });
 
+// Errors in browser console
 export const DefaultWithErrors = template.bind({});
 
-export const Better = template.bind({});
-Better.args = {
+export const WordAround = template.bind({});
+WordAround.args = {
   // Removes error in browser's console, but breaks Control
   dataSource: ELEMENT_DATA,
   // Removes error in browser's console and corrects Control
   displayedColumns: ['position', 'name', 'weight', 'symbol'],
+};
+// Fixes control
+WordAround.argTypes = {
+  dataSource: {
+    type: {
+      name: 'array',
+      value: { name: 'string' },
+    },
+  },
+};
+
+// Errors in browser console
+export const DisableControl = template.bind({});
+DisableControl.argTypes = {
+  dataSource: {
+    control: { disable: true },
+  },
+  displayedColumns: {
+    control: { disable: true },
+  },
+};
+
+// Errors in browser console
+export const SetArgTypesAsStringArray = template.bind({});
+SetArgTypesAsStringArray.argTypes = {
+  dataSource: {
+    type: {
+      name: 'array',
+      value: { name: 'string' },
+    },
+  },
+  displayedColumns: {
+    type: {
+      name: 'array',
+      value: { name: 'string' },
+    },
+  },
+};
+
+// Errors in browser console
+export const SetArgTypesAsOther = template.bind({});
+SetArgTypesAsOther.argTypes = {
+  dataSource: {
+    type: {
+      name: 'other',
+      value: 'string array',
+    },
+  },
+  displayedColumns: {
+    type: {
+      name: 'other',
+      value: 'string array',
+    },
+  },
 };
